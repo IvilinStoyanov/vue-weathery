963012d84f23cdddbd8ef27273e0d4d653128b64
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2023-12-30 19:05:11 +0200

    update pwa settings

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      navigateFallback: '/index.html',
+      runtimeCaching: {
+        urlPattern: new RegExp('^https://api.weatherapi.com/v1/'),
+        handler: 'networkFirst',
+        options: {
+          networkTimeoutSeconds: 20,
+          cacheName: 'api-cache',
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
+        }
+      }
+    },
+  },
 });
