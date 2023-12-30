b37fc4e2cf4a7931eab22d49328a1e9cfc003377
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2023-12-30 19:59:34 +0200

    fixing error in pwa setup config

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       navigateFallback: '/index.html',
-      runtimeCaching: {
-        urlPattern: new RegExp('^https://api.weatherapi.com/v1/'),
-        handler: 'networkFirst',
-        options: {
-          networkTimeoutSeconds: 20,
-          cacheName: 'api-cache',
-          cacheableResponse: {
-            statuses: [0, 200]
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://api.weatherapi.com/v1/'),
+          handler: 'NetworkFirst',
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'api-cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
           }
         }
-      }
-    },
-  },
+      ]
+    }
+  }
 });
